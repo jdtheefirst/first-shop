3cef801633ee6adfd34a656690fb9758a509e3e6
jdtheefirst <jngatia045@gmail.com>
2025-12-05 10:59:20 +0300

    handle cookiestore on your own

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -15,9 +15,7 @@ export async function createClient() {
         setAll(cookiesToSet) {
           try {
             cookiesToSet.forEach(({ name, value, options }) =>
-              cookieStore.set(name, value, {
-                ...options,
-              })
+              cookieStore.set(name, value, options)
             );
           } catch (error) {
             // The `set` method was called from a Server Component.
